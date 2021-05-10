4f9840c03dd3efd61e885ce036258762ac7ac94d
soosbk <66404477+soosbk@users.noreply.github.com>
2021-05-11 03:47:46 +0900

    CFB MODE
    soosbk 나중에 개인 포트폴리오로 활용될 자료입니다. 무단복제 하지마세요
    CFB

---
+++ modeofoperation/CFB.py
@@ -0,0 +1,48 @@
+# soosbk 나중에 개인 포트폴리오로 활용될 자료입니다. 무단복제 하지마세요
+#CFB
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 12:02:29 2020
+
+@author: soosbk
+"""
+import random
+
+def xor_fun(pt,ct):
+    #print(type(pt))
+    new_state=[]
+    for i in range(len(pt)):
+        new_state+=[pt[i]^ct[i]]
+        
+    return new_state
+  
+def get_nonce(): # Nonce 생성 하는 함수
+    nonce = [0 for i in range(16)]
+    for i in range(16):
+        nonce[i] = random.randint(0, 255)
+    return nonce   
+
+def CFB(pt,key):
+    ct=[]
+    #IV=get_nonce();
+    IV=[0xf0,0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,\
+           0xf8,0xf9,0xfa,0xfb,0xfc,0xfd,0xfe,0xfe]   #test
+        
+    block_num=(len(pt)+16-1)//16
+    nonce=IV
+    for i in range(block_num-1):
+        i_th_state=pt[16*i : 16*i+16]
+        state=Toycipher_Enc(nonce,key)
+        nonce=xor_fun(state,i_th_state) 
+        ct+=nonce
+        #xor 한것이 다음 R의 input이 됨
+    
+    #마지막 블록 처리
+    state=Toycipher_Enc(nonce,key)
+    
+    #처리 시작
+    for i in range((len(pt)-16*(block_num-1))&31):
+        ct+=[state[i]^ pt[16*(block_num-1)+i]]
+    
+    return ct
